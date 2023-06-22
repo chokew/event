24673c040d2555aa61a6211c83a300051ceb63ab
chokew <mrwangcl@outlook.com>
2023-06-22 16:26:06 +0800

    nothing

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "event",
   description: "nothing",
+  base: '/event/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
@@ -44,7 +45,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/chokew/event' }
     ]
   }
 })
